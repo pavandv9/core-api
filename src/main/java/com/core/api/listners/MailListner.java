0ef2e3153699fd2a1164a383e36a667a98e6924e
pavandv9 <pavandv9@gmail.com>
2020-05-03 21:26:53 +0530

    handled sending mail

---
+++ src/main/java/com/core/api/listners/MailListner.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package com.core.api.listners;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestNGMethod;
+import org.testng.ITestResult;
+
+import com.core.api.utils.SpringMailUtil;
+
+/**
+ * @author Pavan.DV
+ *
+ */
+public class MailListner implements ITestListener {
+
+	private static String NEW_LINE = System.lineSeparator();
+	private static String FORMAT = "%1$-25s%2$-10s%3$-10s";
+	private static String FORMAT_TEXT = FORMAT + NEW_LINE;
+
+	List<ITestNGMethod> passedtests = new ArrayList<ITestNGMethod>();
+	List<ITestNGMethod> failedtests = new ArrayList<ITestNGMethod>();
+	List<ITestNGMethod> skippedtests = new ArrayList<ITestNGMethod>();
+
+	@Override
+	public void onFinish(ITestContext context) {
+		SpringMailUtil.sendMail(getReport());
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		passedtests.add(result.getMethod());
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		failedtests.add(result.getMethod());
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		skippedtests.add(result.getMethod());
+	}
+
+	private int getTotalTestCase() {
+		return passedtests.size() + failedtests.size() + skippedtests.size();
+	}
+
+	private StringBuilder getReport() {
+		StringBuilder builder = new StringBuilder();
+		builder.append(String.format(FORMAT_TEXT, "Total test case", ":", getTotalTestCase()));
+		builder.append(String.format(FORMAT_TEXT, "Passed test case", ":", passedtests.size()));
+		builder.append(String.format(FORMAT_TEXT, "Failed test case", ":", failedtests.size()));
+		builder.append(String.format(FORMAT_TEXT, "Skipped test case", ":", skippedtests.size()));
+		builder.append(NEW_LINE);
+		if (!failedtests.isEmpty())
+			builder.append(String.format(FORMAT_TEXT, "Failed test methods", ":", failedtests.get(0).getMethodName()));
+		if (!skippedtests.isEmpty())
+			builder.append(
+					String.format(FORMAT_TEXT, "Skipped test methods", ":", skippedtests.get(0).getMethodName()));
+		return builder;
+	}
+
+}
---
+++ src/main/java/com/core/api/utils/SpringMailUtil.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package com.core.api.utils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.springframework.mail.javamail.JavaMailSender;
+
+import com.core.api.constants.MailFile;
+import com.core.api.constants.PropertyFile;
+
+/**
+ * @author Pavan.DV
+ *
+ */
+public class SpringMailUtil implements ILogger {
+
+	static Properties properties;
+
+	public static void sendMail(StringBuilder testcases) {
+		createFileIfNotExist();
+		PropertyUtil.loadProperties(
+				System.getProperty("user.dir") + "/src/main/resources/" + PropertyFile.MAIL_FILE.getValue());
+		if (PropertyUtil.get(MailFile.SEND_MAIL).toLowerCase().equalsIgnoreCase("true")) {
+			EmailConfig emailConfig = new EmailConfig();
+			JavaMailSender mailSender = emailConfig.getJavaMailSender();
+			mailSender.send(emailConfig.emailTemplate(testcases));
+			Logger.logMailRequest();
+		} else {
+			Logger.logMailProperties();
+		}
+	}
+
+	/**
+	 * Create file {@link mail.properties} if doesn't exist in main/resources
+	 * folder.
+	 * 
+	 * @return true if file created.
+	 * @throws IOException
+	 */
+	private static void createFileIfNotExist() {
+		String filePath = System.getProperty("user.dir") + "/src/main/resources/" + PropertyFile.MAIL_FILE.getValue();
+		File file;
+		try {
+			file = new File(filePath);
+			if (file.createNewFile()) {
+				Properties props = PropertyUtil.loadProperties(PropertyFile.MAIL_FILE);
+				properties = props;
+				setDefaultProps();
+				properties.store(new FileOutputStream(filePath), null);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void setDefaultProps() {
+		properties.put(MailFile.SEND_MAIL.getValue(), "false");
+		properties.put(MailFile.HOST.getValue(), "gmail/outlook/office365");
+		properties.put(MailFile.USERNAME.getValue(), "");
+		properties.put(MailFile.PASSWORD.getValue(), "");
+		properties.put(MailFile.TO.getValue(), "");
+		properties.put(MailFile.CC.getValue(), "");
+		properties.put(MailFile.SUB.getValue(), "");
+		properties.put(MailFile.TEXT.getValue(), "");
+	}
+}
