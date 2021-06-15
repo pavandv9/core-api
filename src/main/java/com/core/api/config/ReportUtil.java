dff254279da10f5260106d41e9ad381ae82831ec
pavandv9 <pavandv9@gmail.com>
2021-06-15 12:29:29 +0530

    added extent report

---
+++ src/main/java/com/core/api/config/ReportUtil.java
@@ -0,0 +1,38 @@
+package com.core.api.config;
+
+import static com.core.api.config.ExtentReportManager.getCurrentTest;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.CodeLanguage;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+
+/**
+ * The Class is responsible for Reporting
+ *
+ * @author Pavan.DV
+ * @since 1.3
+ */
+public class ReportUtil {
+
+	public static void logInfo(String details) {
+		getCurrentTest().log(Status.INFO, details);
+	}
+
+	public static void logMessage(Status status, String details) {
+		getCurrentTest().log(status, details);
+	}
+
+	public static void logException(Throwable t){
+		StringWriter stringWriter = new StringWriter();
+		PrintWriter printWriter = new PrintWriter(stringWriter);
+		t.printStackTrace(printWriter);
+		getCurrentTest().fail(MarkupHelper.createCodeBlock(stringWriter.toString()));
+	}
+	
+	public static void logReqRes(Status status, String json) {
+		getCurrentTest().info(MarkupHelper.createCodeBlock(json));
+	}
+}
